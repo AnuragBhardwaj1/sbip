b4c1aec3350b031dd2d5cd56a874116839941348
Anurag Bhardwaj <anurag.bhardwaj@Anurags-MacBook-Pro.local>
2023-09-18 15:58:14 +0530

    initial commit

---
+++ src/main/java/com/manning/sbip/SbipApplication.java
@@ -0,0 +1,13 @@
+package com.manning.sbip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbipApplication.class, args);
+    }
+
+}
